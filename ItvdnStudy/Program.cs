d47dcff78174aa02054aea0339e6772094b17c09
Вячеслав Юрочка <nodokohhik@gmail.com>
2021-11-16 07:14:48 +0300

    Добавьте файлы проекта.

---
+++ ItvdnStudy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ItvdnStudy.Resources.Invoice;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItvdnStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Invoice invoice = new Invoice();    
+            //invoice.Date= DateTime.Parse("17/11/2021 20:00");
+            //invoice.Number = "1";
+            //invoice.From = "Издательство";
+            //invoice.To = "Магазин";
+            //invoice.StorekeeperSurname = "Иванов";
+            //invoice.ForwarderSurname = "Петров";
+
+            Invoice invoice = new Invoice
+            {
+                Date = DateTime.Parse("17/11/2021 20:00"),
+                Number = "1",
+                From = "Издательство",
+                To = "Магазин",
+                StorekeeperSurname = "Иванов",
+                ForwarderSurname = "Петров"
+            };
+
+
+        
+
+           
+            Console.Read();
+ 
+        }
+    }
+}
---
+++ ItvdnStudy/Resources/Class/Invoice/Invoice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItvdnStudy.Resources.Invoice
+{
+    class Invoice
+    {
+        // Title
+        public DateTime Date { get; set; }
+        public string Number { get; set; } = null;
+        public string To { get; set; } = null;
+        public string From { get; set; } = null;
+        // Body
+        public Table Table { get; } = new Table();
+        //Footer
+        public decimal Total { get=>Table.Total; }
+        public string StorekeeperSurname { get; set; } = null;
+        public string ForwarderSurname { get; set; } = null;
+    }
+    class Table
+    {
+        Row[] rows = new Row[0];
+        public int Size => rows.Length;
+        public decimal Total { get; private set; }
+        public void AddRow(Row row)
+        {
+            Array.Resize(ref rows, rows.Length + 1);
+            rows[rows.Length - 1] = row;
+            row.SequentialNumber = rows.Length;
+            Total = +row.Amount;
+        }
+        public Row GetRow(int index) => rows[index];
+    }
+    class Row
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int SequentialNumber { get; set; }
+        public string Description { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Amount
+        {
+            get => Quantity * Price;
+        }
+    }
+
+}
