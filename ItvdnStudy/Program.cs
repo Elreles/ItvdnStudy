5f6525a4078283a096a723ddb3b850fe563f6fbd
Вячеслав Юрочка <nodokohhik@gmail.com>
2021-11-17 07:31:54 +0300

    studing

--- ItvdnStudy/Program.cs
+++ ItvdnStudy/Program.cs
@@ -28,13 +28,40 @@ namespace ItvdnStudy
                 StorekeeperSurname = "Иванов",
                 ForwarderSurname = "Петров"
             };
+            Row row = new Row
+            {
+                Description = "Code complete. Steve McConnel.",
+                Quantity = 1,
+                Price = 100
+            };
+            invoice.Table.AddRow(row);
+            row = new Row
+            {
+                Description = "CLR via C#. Jeffrey Richter.",
+                Quantity = 3,
+                Price = 80
+            };
+            invoice.Table.AddRow(row);
+
+            string stringTitle = $"Дата: {invoice.Date}. \nНомер накладной: {invoice.Number}. " +
+               $"Кому:{invoice.To} От кого:{invoice.From}";
+            Console.WriteLine(stringTitle);
+            for (int i = 0; i < invoice.Table.Size; i++)
+            {
+                row = invoice.Table.GetRow(i);
+                string stringTable = $"{row.SequentialNumber} | {row.Description} | " +
+                   $"{row.Quantity} шт. | {row.Price} руб. | {row.Amount} руб.";
+                Console.WriteLine(stringTable);
+            }
+            Console.WriteLine($"Всего: {invoice.Total} руб.") ;
+            string stringFooter = $"Кладовщик:{invoice.StorekeeperSurname}  \nЭкспедитор:{invoice.ForwarderSurname}";
+
+            Console.WriteLine(stringFooter);
 
 
-        
 
-           
             Console.Read();
- 
+
         }
     }
 }
